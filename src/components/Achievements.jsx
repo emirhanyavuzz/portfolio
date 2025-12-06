3db6040c5822450f7cf32062f6ba45a15e4d14e9
Emirhan Yavuz <emirhanyavuz666@gmail.com>
2025-12-06 23:40:28 +0300

    images added in achievements tab

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -8,7 +8,7 @@ const Achievements = () => {
             title: 'YKS-SAY',
             score: '1180th',
             description: 'University Entrance Exam',
-            icon: <img src={`${import.meta.env.BASE_URL}images/sayisal.jpg`} alt="Profile" className="profile-img" />
+            icon: 'images/say.png'
 
         },
         {
@@ -16,28 +16,28 @@ const Achievements = () => {
             title: 'YKS-SOZ',
             score: '289th',
             description: 'University Entrance Exam',
-            icon: <img src={`${import.meta.env.BASE_URL}images/sozel.png`} alt="Profile" className="profile-img" />
+            icon: 'images/sozel.png'
         },
         {
             id: 3,
             title: 'TOEIC',
             score: '815',
             description: 'Foreign Language Exam (English)',
-            icon: <img src={`${import.meta.env.BASE_URL}images/dil.png`} alt="Profile" className="profile-img" />
+            icon: 'images/dil_bubble.png'
         },
         {
             id: 4,
             title: 'ALES-SAY',
             score: '94',
             description: 'Academic Personnel and Graduate Education Entrance Exam',
-            icon: <img src={`${import.meta.env.BASE_URL}images/sayisal.png`} alt="Profile" className="profile-img" />
+            icon: 'images/sayisal.png'
         },
         {
             id: 5,
             title: 'Bachelor\'s Degree',
             score: '3.02 GPA',
             description: 'Computer Engineering',
-            icon: <img src={`${import.meta.env.BASE_URL}images/tobb_logo.png`} alt="Profile" className="profile-img" />
+            icon: 'images/tobb.png'
         }
     ];
 
@@ -49,7 +49,9 @@ const Achievements = () => {
                 <div className="achievements-grid">
                     {achievements.map((item) => (
                         <div key={item.id} className="achievement-card">
-                            <div className="achievement-icon">{item.icon}</div>
+                            <div className="achievement-icon">
+                                <img src={`${import.meta.env.BASE_URL}${item.icon}`} alt={item.title} />
+                            </div>
                             <div className="achievement-content">
                                 <h3>{item.title}</h3>
                                 <div className="achievement-score">{item.score}</div>
